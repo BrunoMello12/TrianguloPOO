674be87bdaaba3589ecf8e4a29c203cb5b2443e2
bruno <bruno@DESKTOP-C1C4CCP>
2023-04-06 11:55:55 -0300

    Adicionar arquivos de projeto.

---
+++ TrianguloPOO.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using TrianguloPOO.ConsoleApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Triangulo triangulo = new Triangulo();
+
+        Console.WriteLine("Informe o valor de x: ");
+        triangulo.x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o valor de y: ");
+        triangulo.y = int.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o valor de z: ");
+        triangulo.z = int.Parse(Console.ReadLine());
+
+        triangulo.VerificarTriangulo();
+
+    }
+}
---
+++ TrianguloPOO.ConsoleApp/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloPOO.ConsoleApp
+{
+    internal class Triangulo
+    {
+        public int x;
+        public int y;
+        public int z;
+        
+        public void VerificarTriangulo()
+        {
+            if(x < y+z && y < x+y && z < y+x)
+            {
+                if(x == y && y == z)
+                {
+                    Console.WriteLine("Triângulo equilátero");
+                }
+                else if(x == y && y != z || x == z && y != x || y == z && y != x)
+                {
+                    Console.WriteLine("Triângulo isósceles");
+                }
+                else
+                {
+                    Console.WriteLine("Triângulo escaleno");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é um triângulo");
+            }
+        }
+    }
+}
